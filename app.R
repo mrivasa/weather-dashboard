63106a4d43ef1dd18f7e0fd60e9997657a3d0e8c
Miguel Rivas <miguelrivas@southern.edu>
2025-03-12 16:33:07 -0400

    Fixing errors for missing soil moisture. Adding renv package

--- app.R
+++ app.R
@@ -110,7 +110,7 @@ ui <- dashboardPage(
               bsPopover(
                 id = "soil_info_icon",
                 title = "What is soil moisture?",
-                content = paste0("The Davis Instruments sensor measures on a scale of 0 (fully wet) to 200 (fully dry) centibars.  The sensor measures the vacuum created in the soil by the lack of moisture."),
+                content = paste0("The Davis Instruments sensor measures on a scale of 0 (fully wet) to 200 (fully dry) centibars.  The sensor measures the vacuum created in the soil by the lack of moisture. A reading of -1 means that the sensor does not return any reading."),
                 placement = "right",
                 trigger = "click",
                 options = list(container = "body")
@@ -248,7 +248,7 @@ server <- function(input, output, session) {
   })
   outputOptions(output, "total_rows", suspendWhenHidden = FALSE)
 
-  #--- Filter dataframe based on selcted date range
+  #--- Filter data frame based on selected date range
   #--- Caching reactive result based on selected date range
   filtered_weather <- reactive({
     date2 <- input$date_range[2] + 1
@@ -447,22 +447,25 @@ server <- function(input, output, session) {
 
   #--- Getting all weather data
   all_weather <- reactive({
-    fields <- '{"date_and_time":1, "date":1, "soil_moisture_1": "$davis_current_observation.soil_moisture_1"}'
+    fields <- '{"date_and_time":1, "date":1, "soil_moisture_1": { "$ifNull" : ["$davis_current_observation.soil_moisture_1", "N/A"]}}'
     load_data(qry = NULL, fields)
   })
 
   #--- Display daily moisture over time
   output$week_soil <- renderPlotly({
     df_temp <- all_weather()
-    obs <- get_latest_obs()
+    obs <- get_latest_obs() 
 
     by_day <- df_temp %>%
       group_by(date) %>%
-      summarise(avg_moisture = mean(soil_moisture_1))
+      summarise(avg_moisture = mean(soil_moisture_1), na.rm = TRUE) %>%
+      filter(is.na(avg_moisture) == FALSE)
 
     day_avg <- round((obs$soil_moisture_1_day_high + obs$soil_moisture_1_day_low) / 2, 0)
+    day_avg <- ifelse(is.na(day_avg), 0, day_avg)
 
     all_time_avg <- round(mean(df_temp$soil_moisture_1, na.rm = TRUE), 0)
+    all_time_avg <- ifelse(is.na(all_time_avg), 0, all_time_avg)
 
     ggplot() +
       geom_col(by_day, mapping = aes(x = date, y = avg_moisture)) +
